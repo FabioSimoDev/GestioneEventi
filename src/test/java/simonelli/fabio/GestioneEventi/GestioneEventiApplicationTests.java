c9f7a25629f4608e3186fc26e3ff67be8de927f6
FabioSimoDev <fabiofabietto182@gmail.com>
2024-01-26 09:55:40 +0100

    Initial commit

---
+++ src/test/java/simonelli/fabio/GestioneEventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package simonelli.fabio.GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
